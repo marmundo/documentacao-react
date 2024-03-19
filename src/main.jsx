6c517f2fdb925ba2f81d39e58c169b98eb55c0be
Marcelo Damasceno <marcelodamasceno@gmail.com>
2024-03-19 11:37:51 -0300

    Adiciona componentes de Imagem e GeradorDeCoresHex

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Banner from './componentes/Banner/Banner.jsx'
 import Formulario from './componentes/Formulario/Formulario.jsx'
+import Imagem from './componentes/exercicios/Imagem/Imagem.jsx'
+import GeradorDeCoresHex from './componentes/exercicios/GeradorDeCoresHex/GeradorDeCoresHex.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
    <Banner/>
    <Formulario/>
+   <Imagem url="https://placehold.co/200x200" legenda="Legenda" />
+   <GeradorDeCoresHex/>
   </React.StrictMode>,
 )
